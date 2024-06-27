46643f576ef590a456913fd3708103691d877533
Danielyilma <yilmad752@gmail.com>
2024-06-27 15:20:51 +0300

    class attribute getter and setter

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(value) {
+    if (typeof (value) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    this._name = value;
+  }
+
+  set length(value) {
+    if (typeof (value) !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+
+    this._length = value;
+  }
+
+  set students(value) {
+    if (typeof (value) !== 'object') {
+      throw new TypeError('students must be object');
+    }
+
+    this._students = value;
+  }
+}
---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,34 @@
+export default class Currency {
+  constructor(code, name) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(value) {
+    if (typeof (value) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    this._name = value;
+  }
+
+  set code(value) {
+    if (typeof (value) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    this._code = value;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
