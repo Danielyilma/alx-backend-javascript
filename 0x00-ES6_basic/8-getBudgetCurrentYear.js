a5b391813d6212eb1d9fa9557c54b72b1dc9fe11
Danielyilma <yilmad752@gmail.com>
2024-06-28 15:21:34 +0300

    ES6 BASICS

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,14 +1,15 @@
 function getCurrentYear() {
-    const date = new Date();
-    return date.getFullYear();
-  }
-  
-  export default function getBudgetForCurrentYear(income, gdp, capita) {
-    const budget = {};
-  
-    budget[`income-${getCurrentYear()}`] = income;
-    budget[`gdp-${getCurrentYear()}`] = gdp;
-    budget[`capita-${getCurrentYear()}`] = capita;
-  
-    return budget;
-  }
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {};
+  const currentDate = getCurrentYear();
+
+  budget[`income-${currentDate}`] = income;
+  budget[`gdp-${currentDate}`] = gdp;
+  budget[`capita-${currentDate}`] = capita;
+
+  return budget;
+}
