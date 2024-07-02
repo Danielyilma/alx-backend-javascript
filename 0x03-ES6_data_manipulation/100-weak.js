44662907336f42a7142df7bd944f42456dcae9cb
Danielyilma <yilmad752@gmail.com>
2024-07-02 15:27:42 +0300

    data manipulation

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,12 @@
+export const weakMap = new WeakMap();
+
+
+export function queryAPI(endPoint) {
+    const count = weakMap.get(endPoint) || 0;
+
+    if (count >= 5) {
+        throw new Error('Endpoint load is high');
+    }
+
+    weakMap.set(endPoint, count + 1);
+}
