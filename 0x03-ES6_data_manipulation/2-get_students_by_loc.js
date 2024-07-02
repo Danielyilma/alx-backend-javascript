33f110a3f6cdf51282bce0365865731fd91b3903
Danielyilma <yilmad752@gmail.com>
2024-07-02 16:02:11 +0300

    data manipulation

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,6 @@
 export default function getStudentsByLocation(studentArray, city) {
+  if (Array.isArray(studentArray)) {
+    return [];
+  }
   return studentArray.filter((x) => x.location === city);
 }
