93db39fd640f85d72a05b64e6daa4c1f1594858b
Danielyilma <yilmad752@gmail.com>
2024-07-03 04:57:45 +0300

    weak map usage

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,12 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endPoint) {
-  const count = weakMap.get(endPoint) || 0;
-
-  if (count >= 5) {
-    throw new Error('Endpoint load is high');
+  if (weakMap.has(endPoint)) {
+    if (weakMap.get(endPoint) >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+    weakMap.set(endPoint, weakMap.get(endPoint) + 1);
+  } else {
+    weakMap.set(endPoint, 1);
   }
-
-  weakMap.set(endPoint, count + 1);
 }
