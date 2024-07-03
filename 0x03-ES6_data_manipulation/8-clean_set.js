7b5c0d9245412d154ce1a5d7d3a793c083f2e2b2
Danielyilma <yilmad752@gmail.com>
2024-07-03 04:38:15 +0300

    set maniplation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,8 @@
 export default function cleanSet(set, startString) {
   let result = [];
 
+  if (!startString.length || !startString) return '';
+
   for (const key of set) {
     if (startString.length !== 0 && key.startsWith(startString)) {
       result.push(key.slice(startString.length));
