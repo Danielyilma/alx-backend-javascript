7fafbc7720335848c9ad762ef54fb0e180f284e4
Danielyilma <yilmad752@gmail.com>
2024-07-03 04:13:27 +0300

    Typed array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,12 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (length <= position) {
+    throw new Error("Position outside range");
+  }
+
   const buffer = new ArrayBuffer(length);
   const num = new DataView(buffer);
   num.setInt8(position, value);
+  console.log(num.getUint8(position));
 
-  return buffer;
+  return num;
 }
