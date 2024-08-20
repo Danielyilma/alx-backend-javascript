53f57be041224649568ad6f0d3850bf586f6339f
Danielyilma <yilmad752@gmail.com>
2024-08-20 23:08:45 +0300

    express framework

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,42 +4,38 @@ const fs = require('fs');
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
  */
-const countStudents = (dataPath) => {
-  if (!fs.existsSync(dataPath)) {
-    throw new Error('Cannot load the database');
-  }
-  if (!fs.statSync(dataPath).isFile()) {
-    throw new Error('Cannot load the database');
-  }
-  const fileLines = fs
-    .readFileSync(dataPath, 'utf-8')
-    .toString('utf-8')
-    .trim()
-    .split('\n');
-  const studentGroups = {};
-  const dbFieldNames = fileLines[0].split(',');
-  const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
-  for (const line of fileLines.slice(1)) {
-    const studentRecord = line.split(',');
-    const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
-    const field = studentRecord[studentRecord.length - 1];
-    if (!Object.keys(studentGroups).includes(field)) {
-      studentGroups[field] = [];
+function countStudents(path) {
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+  if (!fs.statSync(path).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+
+  const data = fs.readFileSync(path, 'utf-8').trim().split('\n');
+  const fields = data[0].split(',');
+  const studentsData = {};
+
+  for (let i = 1; i < data.length; i += 1) {
+    const student = data[i].split(',');
+    const tempDict = {
+      [fields[0]]: student[0],
+      [fields[1]]: student[1],
+      [fields[2]]: student[2],
+    };
+
+    // console.log(tempDict)
+    if (!studentsData[student.at(-1)]) {
+      studentsData[student.at(-1)] = [];
     }
-    const studentEntries = studentPropNames
-      .map((propName, idx) => [propName, studentPropValues[idx]]);
-    studentGroups[field].push(Object.fromEntries(studentEntries));
+    studentsData[student.at(-1)].push(tempDict);
   }
 
-  const totalStudents = Object
-    .values(studentGroups)
-    .reduce((pre, cur) => (pre || []).length + cur.length);
-  console.log(`Number of students: ${totalStudents}`);
-  for (const [field, group] of Object.entries(studentGroups)) {
-    const studentNames = group.map((student) => student.firstname).join(', ');
-    console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+  console.log(`Number of students: ${data.length - 1}`);
+  for (const [key, value] of Object.entries(studentsData)) {
+    const names = value.map((element) => element[fields[0]], value);
+    console.log(`Number of students in ${key}: ${value.length}. List: ${names.join(', ')}`);
   }
-};
-
+}
 module.exports = countStudents;
