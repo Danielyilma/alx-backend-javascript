46106af250787c98fe98953c2f883b5e30efec72
Danielyilma <yilmad752@gmail.com>
2024-08-21 13:24:46 +0300

    node http server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -5,29 +5,23 @@ const port = 1245;
 
 function captureScriptOutput(path) {
   return new Promise((resolve, reject) => {
-
-    if (path == null){
-      reject(new Error(`Cannot load the database`))
+    if (path == null) {
+      reject(new Error('Cannot load the database'));
     }
     const child = spawn('node', ['./script.js', path]);
 
     let output = '';
-    let error = '';
     child.stdout.on('data', (data) => {
       if (data != null) {
         output += data.toString();
       }
     });
 
-    child.stderr.on('data', (data) => {
-      error += data;
-    });
-
     child.on('close', (code) => {
       if (code === 0) {
         resolve(output);
       } else {
-        reject(new Error(`Cannot load the database`));
+        reject(new Error('Cannot load the database'));
       }
     });
   });
@@ -35,13 +29,14 @@ function captureScriptOutput(path) {
 
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
+    res.statusCode = 200;
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     const title = 'This is the list of our students\n';
     captureScriptOutput(process.argv[2])
       .then((output) => {
-        res.statusCode = 200
-        res.end(title + output.slice(0, -1))
+        res.statusCode = 200;
+        res.end(title + output.slice(0, -1));
       })
       .catch((err) => {
         res.statusCode = 400;
