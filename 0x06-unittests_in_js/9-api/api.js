68259e29f6b7b2e8d172959f3db221a732abd63e
Danielyilma <yilmad752@gmail.com>
2024-08-22 21:51:00 +0300

    test api

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -7,6 +7,17 @@ app.get("/", (req, resp) => {
   resp.send("Welcome to the payment system");
 });
 
+app.get("/cart/:id", (req, res) => {
+  const id = req.params.id;
+  if (!/^[+-]?\d+(\.\d+)?$/.test(id)) {
+    res.statusCode = 400;
+    res.send();
+    return;
+  }
+  res.statusCode = 200;
+  res.send(`Payment methods for cart ${id}`);
+});
+
 app.listen(port, () => {
   console.log("API available on localhost port 7865");
 });
---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,52 @@
+const { expect } = require("chai");
+const request = require("request");
+
+describe("test api", () => {
+  /**
+   * testing / index route
+   */
+
+  describe("testing index page", () => {
+    it("should return 200 status", (done) => {
+      request("http://localhost:7865/", (err, res, body) => {
+        expect(res.statusCode).to.be.equal(200);
+        done();
+      });
+    });
+
+    it("should return Welcome to the payment system body", (done) => {
+      request("http://localhost:7865/", (err, res, body) => {
+        // console.log(body)
+        expect(body).to.be.equal("Welcome to the payment system");
+        done();
+      });
+    });
+
+    it("should return 200 status", (done) => {
+      request("http://localhost:7865/", (err, res, body) => {
+        expect(res.statusCode).to.be.equal(200);
+        done();
+      });
+    });
+  });
+
+  /**
+   * testing /cart/id route
+   */
+
+  describe("cart payment", () => {
+    it("should return 200 status if /cart/id id in number", (done) => {
+      request("http://localhost:7865/cart/1", (err, res, body) => {
+        expect(res.statusCode).to.be.equal(200);
+        done();
+      });
+    });
+
+    it("should return 400 status if /cart/id id in not number", (done) => {
+      request("http://localhost:7865/cart/one", (err, res, body) => {
+        expect(res.statusCode).to.be.equal(400);
+        done();
+      });
+    });
+  });
+});
